c538854036f3dbe383cf10706a7c87754c872fdf
MarinaUtt <marinauteshova13@gmail.com>
2025-04-03 17:11:28 +0500

    feat page main

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin ({
+      template: './index.html',
+      filename: 'index.html'
+    })
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    port: 8000,
+    open: true,
+    hot: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: 'html-loader',
+      },
+      {
+        test: /\.s?css$/,
+        use: ['style-loader', 'css-loader']
+      }  
+    ],     
+  },
+  mode: "development"
+
+};
